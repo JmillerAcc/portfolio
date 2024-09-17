1cb3c88e80a34555e27a1752bea2378918ec32e0
JmillerAcc <125284127+JmillerAcc@users.noreply.github.com>
2024-09-17 15:16:09 -0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Human Machine Interface",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Justin was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Justin's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Justin is the ideal partner.",
+    name: "Larry Barrett",
+    title: "YardGuardian.com",
+  },
+  {
+    quote:
+     "Justin has gone out of his way to make me the best site for my resell crafts and goods site. His attention to detail and dedication to understanding my needs were exceptional. He not only delivered a visually stunning website but also ensured it was user-friendly and optimized for search engines. The increase in traffic and sales since the launch has been remarkable. I highly recommend Justin for anyone looking to elevate their online presence.",
+    name: "Lavonne Griepenstroh",
+    title: "Sales VP of Reselssit.com",
+  },
+  {
+    quote:
+      "We contacted Justin to develop a human machine interface for our new product. His expertise in web development and design was evident from the start. Justin was able to deliver a visually stunning and user-friendly interface that exceeded our expectations. His attention to detail and dedication to understanding our needs were exceptional. We highly recommend Justin for anyone who is looking to interface machines to a web platform and beyond.",
+    name: "Brandon Bracato",
+    title: "Director of Operations Accuride Corporation",
+  },
+  {
+    quote:
+      "Higly skilled and professional. Justin has been a pleasure to work with. He has a great eye for design and is very knowledgeable in his field. He was able to exceeded our expectations. I highly recommend Justin for anyone looking into web development as a platform to reach users",
+    name: "Josh Lagrange",
+    title: "Pastor of True Vine Baptist Church",
+  },
+  {
+    quote:
+      "Justin did an amazing job with our ecommerace store. He was able to deliver a visually stunning and user-friendly interface that exceeded our expectations. His attention to detail and dedication to understanding our needs were exceptional. We highly recommend Justin for anyone who is looking to elevate their online presence.",
+    name: "Justin Johnson",
+    title: "BuyItNow.com",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Assistant Controls Engineer - Spencer Plastics",
+    desc: "Assisted in the development of Machine-Based platforms using VBA, FactoryTalk ViewStudio and PLC programing.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer - Freelance",
+    desc: "Designed and develped web applications for clients using modern frontend technologies in many different fields.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Controls Engineering - Accuride Corporation",
+    desc: "Developed and maintained full stack using modern technologies. Such as, a full Next.Js react front end intergrated with .NET machine interface logic.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "More Freelance Work",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d019f7ef9238a6151821d5cd481b1430@o4507968320897024.ingest.us.sentry.io/4507968327843840",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
